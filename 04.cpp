2e6e36643af52cf459e9a1ac202703e17d0749f8
znm3m8 <znm3m8@mst.edu>
2017-05-05 18:24:11 -0500

    Completed 0-4

---
+++ 04.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	cout << "#include <iostream\n\nint main()\n{\n";
+	cout << "\tstd::cout << \"Hello, World\" << endl;\n";
+	cout << "\treturn 0;\n}";
+	return 0;
+}
