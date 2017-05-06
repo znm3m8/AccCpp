7b98f90b7519556bf091e117ad85bd83192481db
znm3m8 <znm3m8@mst.edu>
2017-05-05 22:15:52 -0500

    Completed 2-0

---
+++ chap2/2_0.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <string>
+
+using std::cout;
+using std::cin;
+using std::string;
+using std::endl;
+
+int main()
+{
+	cout << "Please enter your first name: ";
+
+	string name;
+	cin >> name;
+	 
+	const string greeting  = "Hello, " + name + "!";
+	
+	const int pad = 5;
+	const int rows = 2*pad + 3;
+	for(int r = 0; r < rows; r++)
+	{
+		const string::size_type cols = greeting.size() + pad*2 + 2;
+		int c = 0;
+		cout << endl;
+
+		while(c < cols)
+		{
+			if(r == pad + 1 && c == pad + 1)
+			{
+				cout << greeting;
+				c += greeting.size();
+			}
+			else 
+			{
+				if(r == 0 || r == rows - 1 || c == 0 || c == cols - 1 )
+				{
+					cout << "*";
+				}
+				else
+				{
+					cout << " ";
+				}
+				c++;		
+			}
+		}
+	}
+	return 0;
+}
