42f232ea481cf3af0fe5259abb15c7292e6075c6
znm3m8 <znm3m8@mst.edu>
2017-05-06 00:47:18 -0500

    Completed 2-7

---
+++ chap2/2_7.cpp
@@ -0,0 +1,13 @@
+#include <iostream> 
+
+using std::cout;
+using std::endl;
+
+int main()
+{
+	for(int i = 10; i>=-5; i--)
+	{
+		cout << i << endl;
+	}
+	return 0;
+}
--- test.cpp
+++ test.cpp
@@ -5,8 +5,11 @@ using namespace std;
 
 int main()
 {
-	string name;
-	name  = "Hello, " + "World";
-	cout << name << endl;
+	int i = 0;
+	while(i < 10)
+	{
+		i+=1;
+		cout << i << endl;
+	}
 	return 0;
 }
